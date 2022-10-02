e65001e89eec8e680ac1160cf3088ecb52fa9cab
Yash Pandey <yashpandey2727@gmail.com>
2022-10-02 14:16:34 +0530

    added process.env.PORT to add.listen in app.js and added an Li in navbar in header.ejs

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.get("/compose", function(req,res){
   res.render("compose");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
